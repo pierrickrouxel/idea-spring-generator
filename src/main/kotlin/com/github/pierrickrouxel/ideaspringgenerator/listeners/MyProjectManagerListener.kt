3e5aa45b04dab436211280822cc69d7add955bd4
GitHub Action <action@github.com>
2020-12-14 12:17:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/pierrickrouxel/ideaspringgenerator/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/pierrickrouxel/ideaspringgenerator/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pierrickrouxel.ideaspringgenerator.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pierrickrouxel.ideaspringgenerator.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/pierrickrouxel/ideaspringgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pierrickrouxel.ideaspringgenerator.services
+
+import com.github.pierrickrouxel.ideaspringgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
